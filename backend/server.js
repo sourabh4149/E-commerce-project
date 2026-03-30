5d2458cc3ab9fb5a5516239f198e1796b060524b
Sourabh Yadav <Sourabhyadav9560@gmail.com>
2026-03-30 15:10:38 +0530

    Enable CORS with wildcard origin for API access

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,13 @@ connectDB()
 connectCloudinary()
 
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
+
+app.use(cors({
+  origin: "*"
+}))
+
+
 
 // Api endpoints
 app.use('/api/user',userRouter);
